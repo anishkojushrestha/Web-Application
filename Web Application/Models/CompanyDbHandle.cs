7938d24509d89b1088baafdfef7b3f57166d4f4b
anishkojushrestha <anishkoju4@gmail.com>
2023-05-30 14:35:27 +0545

    insert company and contact person

--- Web Application/Models/CompanyDbHandle.cs
+++ Web Application/Models/CompanyDbHandle.cs
@@ -25,12 +25,22 @@ namespace Web_Application.Models
             str.Append(" set @cid = (select isnull(max(id), 0) + 1 from CompanyInfo) \n");
             str.Append(" INSERT INTO CompanyInfo(id,CompanyName, Email, PanNumber, Address, City, RegistrationDate, AchiveFrom, AchiveTo ) VALUES(@cid,'" +vm.CompanyName+"','" +vm.Email+"',"+vm.PanNumber+",'"+vm.Address+"','"+vm.City+"','"+ registerDate + "','"+ AchiveFrom + "','"+ AchiveTo + "') \n");
             str.Append("declare @pid bigint\n");
-            str.Append("set @pid = (select isnull(max(id),0)+1 from ContactPerson)\n");
-            foreach(var data in vm.contactPersonVM)
-            {
-                
-                str.Append("INSERT INTO ContactPerson(contactid, contactname, phonenumber, mobilenumber, companyid ) VALUES(@pid, '" + data.ContactName + "'," + data.phoneNumber + "," + data.MobileNumber + ",@cid)\n");
 
+            foreach (var data in vm.contactPersonVM)
+            {
+
+                var list = new ContactPersonVM()
+                {
+                    ContactName = data.ContactName,
+                    MobileNumber = data.MobileNumber,
+                    phoneNumber = data.phoneNumber
+                };
+                
+                str.Append("set @pid = (select isnull(max(ContactId),0)+1 from ContactPerson)\n");
+
+                str.Append("INSERT INTO ContactPerson(ContactId, ContactName, PhoneNumber, MobileNumber, CompanyId ) VALUES(@pid, '" + data.ContactName + "'," + data.phoneNumber + "," + data.MobileNumber + ",@cid) \n");
+
+                
             }
 
            
